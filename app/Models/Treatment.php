d36d0d3be44c1f74cec1e96cd418fba053177353
Asif Mostofa Sazid <asif83sazid@gmail.com>
2025-06-17 00:23:51 +0600

    Filament Installation and Getting start is completed

---
+++ app/Models/Treatment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Casts\MoneyCast;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Treatment extends Model
+{
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
+    
+    protected $guarded = [];
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
+}
